e77773476b3793717293bb6846f8bb0014a6a824
mohamed elmi <mohamed.elmi@luulsolutions.com>
2017-04-28 00:04:02 +0600

    cloud code added

--- cloud/main.js
+++ cloud/main.js
@@ -1,4 +1,20 @@
 
-Parse.Cloud.define('hello', function(req, res) {
-  res.success('Hi');
+Parse.Cloud.define('prayer', function(req, res) {
+
+  var prayerurl = 'http://api.aladhan.com/calendar?latitude=23.8103&longitude=90.4125&timezonestring=Asia%2FDhaka&method=2&month=05&year=2017';
+   var header = { 'Content-Type': 'application/x-www-form-urlencoded' }
+   console.log(prayerurl);
+  Parse.Cloud.httpRequest({
+        method: 'GET',
+        url: prayerurl,
+        headers: header,
+    }).then(function (httpResponse) {
+            console.log("done")
+          console.log(httpResponse.data)
+        res.success(httpResponse.data);
+    }, function (httpResponse) {
+        console.log(httpResponse.data);
+        res.error(httpResponse.data);
+    });
+
 });
