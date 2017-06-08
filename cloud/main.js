81a239f363d27cc05857d4d2b56bde1b0058bac7
mohamed elmi <mohamed.elmi@luulsolutions.com>
2017-06-08 20:55:03 +0600

    added other cloud code  funcations

--- cloud/main.js
+++ cloud/main.js
@@ -25,3 +25,27 @@ console.log(month,year);
 
 
 });
+
+
+Parse.Cloud.define('mosques', function(request, res) {
+
+    
+ var _url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=' +parseFloat(request.params.lat) + ',' +parseFloat(request.params.lon) + '&rankby=' + request.params.rankby + '&types=' + request.params.type + '&key= ' + request.params.key
+
+   var header = { 'Content-Type': 'application/x-www-form-urlencoded' }
+   console.log(_url);
+  Parse.Cloud.httpRequest({
+        method: 'GET',
+        url: _url,
+        headers: header,
+    }).then(function (httpResponse) {
+            console.log("done")
+          console.log(httpResponse.data)
+        res.success(httpResponse.data);
+    }, function (httpResponse) {
+        console.log(httpResponse.data);
+        res.error(httpResponse.data);
+    });
+
+
+});
